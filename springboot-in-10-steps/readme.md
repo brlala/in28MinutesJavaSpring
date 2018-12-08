ec50ef5416d4c5b5d160dcaf8619d0ba64bc7122
User <User@DESKTOP-MTVDDJD>
2018-12-08 23:45:46 +0800

    Spring Boot autoconfig with HAL Browser

---
+++ springboot-in-10-steps/readme.md
@@ -0,0 +1,3 @@
+Using SpringBoot allows autoconfiguration. It will look at the framework, and files used to configure the needed dependencies.
+
+disable logging.level.org.springframework: DEBUG; in application.properties to disable debug log.
--- springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
+++ springboot-in-10-steps/src/main/java/com/in28minutes/springboot/basics/springbootin10steps/BooksController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController //use to identify controller
-public class BookController {
+public class BooksController {
 	@GetMapping("/books") //controller for the URL
 	public List<Book> getAllBooks(){
 		return Arrays.asList(new Book(1, "Mastering Spring 5.0", "Rangga"));
