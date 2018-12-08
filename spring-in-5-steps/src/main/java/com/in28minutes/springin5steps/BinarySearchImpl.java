6bbd3b4ce1f3b2df14ff81ed58c12a6049084a33
User <User@DESKTOP-MTVDDJD>
2018-12-08 17:05:31 +0800

    dynamic wiring, Spring will create all @Component and tries to wire them to the @AutoWired which implements it

--- spring-in-5-steps/src/main/java/com/in28minutes/springin5steps/BinarySearchImpl.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/springin5steps/BinarySearchImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Component;
 public class BinarySearchImpl {
 
 	@Autowired
+	//it will find classes with @Component that implements sortAlgorithm
 	private SortAlgorithm sortAlgorithm;
 	
 	
