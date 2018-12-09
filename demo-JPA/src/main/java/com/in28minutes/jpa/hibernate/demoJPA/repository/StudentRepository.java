37321a4099c7506e893333902a0738971f333d79
User <User@DESKTOP-MTVDDJD>
2018-12-09 22:49:30 +0800

    @ManyToMany insertion, need to add into list of both fields then persist it

--- demo-JPA/src/main/java/com/in28minutes/jpa/hibernate/demoJPA/repository/StudentRepository.java
+++ demo-JPA/src/main/java/com/in28minutes/jpa/hibernate/demoJPA/repository/StudentRepository.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import com.in28minutes.jpa.hibernate.demoJPA.entity.Course;
 import com.in28minutes.jpa.hibernate.demoJPA.entity.Passport;
 import com.in28minutes.jpa.hibernate.demoJPA.entity.Student;
 
@@ -15,33 +16,55 @@ import com.in28minutes.jpa.hibernate.demoJPA.entity.Student;
 @Transactional
 public class StudentRepository {
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
 	EntityManager em;
-	
-	 public Student findById(Long id) {
-		 return em.find(Student.class, id);
-	 }
-	 
-	 public void deleteById(Long id) {
-		 Student student = findById(id);
-		 em.remove(student);
-	 }
-	 
-	 public Student save(Student student) {
-		 if (student.getId()==null)
-			 em.persist(student);
-		 if (student.getId()!=null)
-			 em.merge(student);
-		 return student;
-	 }
-	 
-	 public void saveStudentWithPassport() {
-		 Passport passport = new Passport("Z123456");
-		 em.persist(passport);
-		 
-		 Student student = new Student("Mike");
-		 student.setPassport(passport);
-		 em.persist(student);
-	 }
+
+	public Student findById(Long id) {
+		return em.find(Student.class, id);
+	}
+
+	public void deleteById(Long id) {
+		Student student = findById(id);
+		em.remove(student);
+	}
+
+	public Student save(Student student) {
+		if (student.getId() == null)
+			em.persist(student);
+		if (student.getId() != null)
+			em.merge(student);
+		return student;
+	}
+
+	public void saveStudentWithPassport() {
+		Passport passport = new Passport("Z123456");
+		em.persist(passport);
+
+		Student student = new Student("Mike");
+		student.setPassport(passport);
+		em.persist(student);
+	}
+
+	public void insertHardCodedStudentAndCourse() {
+		Student student = new Student("Jack");
+		Course course = new Course("Microservices in 100 steps");
+		em.persist(student);
+		em.persist(course);
+
+		student.addCourse(course);
+		course.addStudent(student);
+
+		// Persist the owning side afterwards
+		em.persist(student);
+	}
+
+	//Generic method
+	public void insertStudentAndCourse(Student student, Course course) {
+		student.addCourse(course);
+		course.addStudent(student);
+
+		em.persist(student);
+		em.persist(course);
+	}
 }
